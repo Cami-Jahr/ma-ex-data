ca7271d36c19ba1bec2f544dc59280ae7f5865b9
Camilla Jahr <camilla.jahr@hotmail.com>
2025-04-06 00:18:34 +0200

    rename

--- wiki/main.py
+++ wiki/main.py
@@ -1,7 +1,7 @@
-import create_character_pages
-import create_kioku_pages
+import character_pages
+import kioku_pages
 
 
 if __name__ == "__main__":
-    create_character_pages.create_character_pages()
-    create_kioku_pages.create_kioku_pages()
+    character_pages.create_character_pages()
+    kioku_pages.create_kioku_pages()
