2e36a2df198278d9dd2af886765bcc03d4fdd4a3
Camilla Jahr <camilla.jahr@hotmail.com>
2025-04-10 21:57:12 +0200

    Adds more items

--- decrypt_and_copy_files.py
+++ decrypt_and_copy_files.py
@@ -3,6 +3,7 @@ import hashlib
 import json
 import os
 from collections import defaultdict
+import manifests.helpers as helpers
 
 
 def generate_bytes_key(key, salt=""):
@@ -34,13 +35,15 @@ for manifest in (
         dump = json.load(f)
 
     path_dict = {
-        item["pathId"]: item["path"] for item in dump["payload"]["pathMappingMstList"]
+        item["pathId"]: {"path": item["path"], "taken": False}
+        for item in dump["payload"]["pathMappingMstList"]
     }
     mstlist = dump["payload"]["mstList"]
 
-    for key, mon in enumerate(mstlist):
+    for mon in mstlist:
         salt = "1c7f"
-        name = path_dict.get(mon["pathId"], "Path not found") + mon["name"]
+        name = path_dict[mon["pathId"]]["path"] + mon["name"]
+        path_dict[mon["pathId"]]["taken"] = True
         input_str = name.split("/")[-1] + salt
         input_bytes = input_str.encode("utf-8")
         md5_hash = hashlib.md5(input_bytes).digest()
@@ -51,26 +54,45 @@ for manifest in (
             .replace("/", "_")
             .rstrip("=")
         )
-        byte_key = generate_bytes_key(name + mon["cryptoKey"], "")
-        file_keys[base64_str] = byte_key
+        if "isEncrypted" not in mon or mon["isEncrypted"]:
+            byte_key = generate_bytes_key(name + mon["cryptoKey"], "")
+            file_keys[base64_str] = byte_key
         file_names[base64_str] = name
 
 
 def copy_files(target_folder: str, cwd: str):
     occurrences: dict[str, int] = defaultdict(int)
-    known_types = (b"CRID", b"AFS2", b"@UTF")
     for root, _, files in os.walk(cwd):
         lroot = root.replace("/", "\\").split("\\")
         for filename in files:
             target_path = file_names.get(filename)
             if target_path:
+                if "thumb" not in filename:
+                    del file_names[filename]
                 folders = target_path.split("/")
                 known_mappings[filename] = folders[-1]
                 for i in range(1, len(folders)):
                     known_mappings[lroot[-i]] = folders[-i - 1]
 
+    backward_mappings = {v: k for k, v in known_mappings.items()}
+    for k, v in file_names.items():
+        print(
+            f"{k} - {v:70} - {"/".join([backward_mappings.get(p, p) for p in v.split("/")[:-1]])
+            + "/"
+            + k:70}"
+        )
+
+    return
+
     for root, _, files in os.walk(cwd):
         for filename in files:
+            target_list = os.path.join(root, filename).lstrip(cwd).split(os.path.sep)
+
+            for i, val in enumerate(target_list):
+                target_list[i] = known_mappings.get(val, val)
+
+            if os.path.isfile(os.path.join(target_folder, *target_list)):
+                continue
             with open(os.path.join(root, filename), "rb") as f:
                 data = bytearray(f.read())
 
@@ -78,26 +100,16 @@ def copy_files(target_folder: str, cwd: str):
                 file_key = file_keys.get(filename)
                 data_type = bytes(data[:4])
                 occurrences[str(data_type)] += 1
-                if file_key and data_type not in known_types:
-                    # usm, acb and awb files are only common patterns I've found which are not encrypted, just the name is obfuscated
+                if file_key:
                     # XOR each byte with the key for the encrypted ones
                     for i in range(len(data)):
                         data[i] ^= file_key[i % len(file_key)]
             except KeyError:
                 print("INFO: Using obfuscated name for", os.path.join(root, filename))
 
-            target_list = os.path.join(root, filename).lstrip(cwd).split(os.path.sep)
-
-            for i, val in enumerate(target_list):
-                target_list[i] = known_mappings.get(val, val)
-
             os.makedirs(os.path.join(target_folder, *target_list[:-1]), exist_ok=True)
             with open(os.path.join(target_folder, *target_list), "wb") as f:
                 f.write(data)
-
-    for k, v in occurrences.items():
-        if v > 1 and k not in (str(s) for s in known_types):
-            print(f"MISSING KNOWN TYPE? {v} occurrences of {k} were spotted")
 
 
 if __name__ == "__main__":
